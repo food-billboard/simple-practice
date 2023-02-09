7aa0c447a31a51731b20314aaaa7cea40238230c
food-billboard <50391608+food-billboard@users.noreply.github.com>
2023-02-09 10:21:13 +0800

    feat(flag-getter): :tada:

---
+++ packages/flag-getter/src/App/index.tsx
@@ -0,0 +1,12 @@
+import type { Component } from 'solid-js';
+import styles from './index.module.css';
+
+const App: Component = () => {
+  return (
+    <div class={styles['flag-getter']}>
+      hello world 
+    </div>
+  );
+};
+
+export default App;
